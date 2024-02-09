c8ee6ebd40c3140caa9393e49f66254f8c52f0fc
rootcat <103029185+rootcathacking@users.noreply.github.com>
2024-02-09 10:40:28 +0100

    Update awscracknip.sh
    minor changes, next up support for non-default profile

--- awscracknip.sh
+++ awscracknip.sh
@@ -4,10 +4,11 @@ stackname=awscracknip   #Rename if you want another name for the stack
 case "$1" in
   "-install")
     key=$2
-    instance=${3:-g4dn.xlarge}
+    instance=${3:-g5.xlarge}
+  #  profile=${4:-default}
 
     echo "Deploying stack"
-    aws cloudformation create-stack --template-body file://crack.yaml --stack-name $stackname --parameters ParameterKey=KeyName,ParameterValue=$key ParameterKey=InstanceType,ParameterValue=$instance --output text
+    aws cloudformation create-stack --template-body file://crack.yaml --stack-name $stackname --parameters ParameterKey=KeyName,ParameterValue=$key ParameterKey=InstanceType,ParameterValue=$instance --output text 
     sleep 2
     echo "Stack is in creation, wait a few seconds. You can check stack status via"
     echo "-info"
@@ -18,13 +19,13 @@ case "$1" in
   "-start")
     echo "Start order is given"
     instanceid=$(aws cloudformation describe-stacks --stack-name $stackname --query Stacks[*].Outputs[0].OutputValue --output text)
-    aws ec2 start-instances --instance-ids $instanceid
+    aws ec2 start-instances --instance-ids $instanceid 
     ;;
 
   "-stop")
     echo "Instance is stopped"
     instanceid=$(aws cloudformation describe-stacks --stack-name $stackname --query Stacks[*].Outputs[0].OutputValue --output text)
-    aws ec2 stop-instances --instance-ids $instanceid
+    aws ec2 stop-instances --instance-ids $instanceid 
     ;;
 
   "-kill")
@@ -43,16 +44,16 @@ case "$1" in
   "-connect")
     stringec2=$(aws cloudformation describe-stacks --stack-name $stackname --query Stacks[*].Outputs[1].OutputValue --output text)
     setkey=$(aws cloudformation describe-stacks --stack-name awscracknip --query Stacks[*].Parameters[0].ParameterValue --output text)
-    ssh -i $setkey.pem ubuntu@$stringec2
+    ssh -i $setkey.pem admin@$stringec2
     ;;
 
   *)
     echo "Usage of awscracknip"
     echo "First -install, then you can -connect, display -info, -stop and -start the ec2 instance and -kill the stack."
     echo "-install needs a key, and you can select the ec2 instance, by default"
-    echo "a g4dn.xlarge instance gets deployed"
+    echo "a g5.xlarge instance gets deployed"
     echo "Example usage:"
-    echo "bash awscracknip.sh -install ssh-keyname g3.8xlarge"
+    echo "bash awscracknip.sh -install ssh-keyname t2.small"
     exit 1
     ;;
 esac
